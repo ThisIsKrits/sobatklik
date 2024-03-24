6ae293c7b7100f6a306174524802de7fa023ffd2
ThisIsKrits <rieflvi@gmail.com>
2024-03-24 20:57:24 +0700

    fix: add nullable phone database migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('fullname');
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->date('birthdate');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
